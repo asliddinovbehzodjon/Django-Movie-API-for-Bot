604d4d709d2f7a6cc5d93035747dc8ec117ff0e3
Behzod <behzodtuit@gmail.com>
2023-03-08 20:01:23 +0500

    deploy

--- project/settings.py
+++ project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-()dz6*#mw1dd2wmp-)62vjyyqhivhrk#ol!(k^8=gcm9xh9x!_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.pythonanywhere.com']
 
 
 # Application definition
