e275934be550a94ac2f6bfd737d81f468d8cd1b7
Behzod <behzodtuit@gmail.com>
2023-03-08 20:06:24 +0500

    deploy

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework.response import Response
+from rest_framework.filters import SearchFilter
 from rest_framework import status
 # Create your views here.
 from .serializer import *
@@ -8,6 +9,8 @@ from rest_framework.viewsets import ModelViewSet
 class MovieViewset(ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['id']
     def create(self, request, *args, **kwargs):
         data = request.data
         movie  = Movie.objects.create(name=data['name'],file_id=data['file_id'])
